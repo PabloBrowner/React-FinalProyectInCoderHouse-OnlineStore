a296a51dc44697b80c2d9e87fd4675284876c56d
Pablo Browner <pablobrowner@gmail.com>
2022-07-07 22:48:28 -0300

    "commit de archivos"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
+    <>
     <NavBar/>
+    <ItemListContainer greeting={"Este es el catalogo de lamparas"}/>
+    </>
   );
 }
 
